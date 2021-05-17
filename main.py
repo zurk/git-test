35de3257c32dc5e20655e292ab59cbdef949c2c1
k <kslavnov@gmail.com>
2021-05-17 10:54:56 +0300

    Add argument parser

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-lower = 900
-upper = 1000
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("lower", type=int,  help="Number to start generation from.")
+    parser.add_argument("upper", type=int, help="Number to end generation.")
+
+    return parser
+
+
+parser = get_parser()
+args = parser.parse_args()
+
+lower = args.lower
+upper = args.upper
 
 print("Prime numbers between", lower, "and", upper, "are:")
 
